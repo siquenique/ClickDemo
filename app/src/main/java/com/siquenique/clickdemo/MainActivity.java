98438019e58b76fec094967b2bc14917aa5f4c16
Joao Siquenique <siquenique@gmail.com>
2017-03-23 10:46:56 +0100

    ClickApp
    App done with android studio

---
+++ app/src/main/java/com/siquenique/clickdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.siquenique.clickdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void buttonClick (View view) {
+
+        Log.i("Info" , "Button was clicked");
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
